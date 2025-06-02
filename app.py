00e4e79967ca791b9a1fae22d2bf2d5e4d4a7b27
zoltanas <108790802+zoltanas@users.noreply.github.com>
2025-06-02 17:21:29 +0300

    report

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request, redirect, url_for, flash, send_file, jsonify, Response, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
